484bce25bfa4058336008ba4c46d700878506283
Torsten Dittmann <torsten.dittmann@googlemail.com>
2026-03-04 22:48:57 +0400

    Set static flag for routes in Tailwind router

--- examples/tailwind/src/router.ts
+++ examples/tailwind/src/router.ts
@@ -4,14 +4,17 @@ export const router = create_router([
 	{
 		path: "/",
 		component: () => import("./routes/index.svelte"),
+		static: true,
 	},
 	{
 		path: "/about",
 		component: () => import("./routes/about.svelte"),
+		static: true,
 	},
 	{
 		path: "/user/:id",
 		component: () => import("./routes/user.svelte"),
+		static: false,
 	},
 ]);
 
